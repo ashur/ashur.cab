ecd0e91f96d40ecef9fea71ce44c5e8130216a7b
Ashur Cabrera <hello@ashur.cab>
2018-09-05 17:06:20 -0700

    Add :prod tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,32 @@ const gulp = require( 'gulp' );
 const sass = require( 'gulp-sass' );
 const serve = require( 'gulp-serve' );
 
+/* HTML */
 gulp.task( 'html', () =>
 {
 	return gulp.src('./src/html/*.html' )
 	    .pipe( gulp.dest( './dist' ) );
 });
 
+/* Style */
 gulp.task( 'style', () =>
 {
 	return gulp.src('./src/style/main.scss' )
 	    .pipe( sass().on( 'error', sass.logError ) )
 	    .pipe( gulp.dest( './dist' ) );
 });
+gulp.task( 'style:prod', () =>
+{
+	return gulp.src('./src/style/main.scss' )
+	    .pipe( sass({ outputStyle: 'compressed' }).on( 'error', sass.logError ) )
+	    .pipe( gulp.dest( './dist' ) );
+});
 
+/* Build */
 gulp.task( 'build', ['html','style'] );
+gulp.task( 'build:prod', ['html', 'style:prod'] );
 
+/* Watch */
 gulp.task( 'watch', () =>
 {
 	gulp.watch( 'src/html/*.html', ['html'] );
