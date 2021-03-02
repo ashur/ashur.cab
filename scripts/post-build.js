79a9e5380b5adc178b1d6625d329305113fab879
Ashur Cabrera <hello@ashur.cab>
2021-03-02 15:16:31 -0700

    Switch to real ./dist size

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,5 +1,47 @@
+const fs = require( "fs" );
 const fsUtils = require( "nodejs-fs-utils" );
 
-let stat = fsUtils.fsizeSync( "./dist" );
-console.log( stat );
+function getSize( dirName, token )
+{
+	let originalSize = fsUtils.fsizeSync( dirName );
+	let finalSize = originalSize - token.length + "1023-abcdbyte".length;
 
+	let i = 0;
+	while( finalSize / 1024 > 1 )
+	{
+		finalSize = finalSize / 1024;
+		i++;
+	}
+
+	let suffixes = {
+		0: "byte",
+		1: "kilobyte",
+		2: "megabyte"
+	}
+
+	return `${ Math.floor( finalSize ) }-${ suffixes[i] }`;
+}
+
+function replaceToken( token, size )
+{
+	let filePath = "./dist/index.html";
+
+	try
+	{
+		let contents = fs.readFileSync( filePath );
+		let newContents = contents
+			.toString()
+			.replace( token, size );
+
+		fs.writeFileSync( filePath, newContents );
+	}
+	catch( error )
+	{
+		console.log( error );
+		process.exit( 1 );
+	}
+}
+
+let token = "{size}";
+let finalSize = getSize( "./dist", token );
+replaceToken( token, finalSize );
